de348ebb93a5693e3526334e98cd8a74972275da
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-24 20:11:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,4 +9,15 @@ attr_reader :id
     @id = id
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grage INTEGER
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
 end
